fc338d314f9a8184a2baec7ece2996e703d9025b
Irreq <irreq@protonmail.com>
2023-09-01 19:56:45 +0200

    Added antenna in c

---
+++ backend/src/antenna.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _Antenna {
+    float a;
+} Antenna;
+
+
+int main(int argc, char *argv[])
+{
+    Antenna antenna;
+
+    printf("Starting antenna\n");
+    return EXIT_SUCCESS;
+}
