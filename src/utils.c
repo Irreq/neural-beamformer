fb667628356a2c5f34cf6ddacd3491d777de2123
Irreq <irreq@protonmail.com>
2023-10-08 17:39:39 +0200

    Fixed twos-complement and normalization

--- src/utils.c
+++ src/utils.c
@@ -1,10 +1,24 @@
 #include <stdint.h>
 #include <limits.h>
 
-float normalize_int32(int32_t value) {
-    return value < 0
-        ? -((float)value) / INT_MIN
-        : ((float)value) / INT_MAX;
+#define INT24_MIN (-8388608)  // -2^23
+#define INT24_MAX (8388607)   // 2^23 - 1
+
+
+float normalize_int32(int32_t value24Bit) {
+    // Mask to keep only the 24 least significant bits
+    value24Bit &= 0xFFFFFF;
+
+    // Check the sign bit (bit 23)
+    if (value24Bit & 0x800000) {
+        // It's a negative number
+        // Calculate two's complement
+        value24Bit -= 0x1000000;
+    }
+    
+    return value24Bit < 0
+        ? -((float)value24Bit) / INT24_MIN
+        : ((float)value24Bit) / INT24_MAX;
 }
 
 int32_t denormalize_int32(float normalized_value) {
